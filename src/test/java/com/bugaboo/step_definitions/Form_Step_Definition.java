a95ccaf177899065524c668d1a03f7054cac7296
Magbule Bamja <magbule.bamja@gmail.com>
2023-09-03 07:38:42 +0200

    changes in step definition

--- src/test/java/com/bugaboo/step_definitions/Form_Step_Definition.java
+++ src/test/java/com/bugaboo/step_definitions/Form_Step_Definition.java
@@ -25,59 +25,19 @@ public class Form_Step_Definition {
     }
     @When("choose {string}")
     public void choose(String purchaseLocation) {
-        List<WebElement>onlineOrShop= Driver.getDriver().findElements(By.xpath("//div[@class=\"slds-form-element__control\"]/span"));
-if(onlineOrShop.get(0).getText().equalsIgnoreCase("Online")){
-    onlineOrShop.get(0).click();
-}else{
-    onlineOrShop.get(1).click();
-}
-
-    }
-    @When("writing {string}")
-    public void writing(String description) {
-        formPage.description.sendKeys(description);
-
-    }
-    @When("verify {string}")
-    public void verify(String firstName) {
-        formPage.firstName.sendKeys();
+        List<WebElement> onlineOrShop = Driver.getDriver().findElements(By.xpath("//div[@class=\"slds-form-element__control\"]/span"));
+        if (onlineOrShop.get(0).getText().equalsIgnoreCase("Online")) {
+            onlineOrShop.get(0).click();
+        } else {
+            onlineOrShop.get(1).click();
+        }
     }
 
-    @When("filling the {string}")
-    public void filling_the(String lastName) {
+        @Then("verify user should see {string}")
+        public void verify_user_should_see (String string){
+            formPage.lastName.sendKeys(string);
 
-    }
-
-    @When("filling {string}")
-    public void filling(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("choose {string}")
-    public void choose(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("writing {string}")
-    public void writing(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-    @When("verify {string}")
-    public void verify(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        }
     }
 
 
-    @Then("verify user should see {string}")
-    public void verify_user_should_see(String lastName) {
-        formPage.lastName.sendKeys(lastName);
-
-    }
-
-
-}
--- src/test/resources/features/form.feature
+++ src/test/resources/features/form.feature
@@ -21,6 +21,8 @@ Feature: Filling the form in Product Registration
     And filling "<Country>"
     Then verify user should see "Thank you for contacting us about your product. You will receive a confirmation in your email. We will process your request as soon as possible."
 
+
+    #Valid inputs
 Examples:
   | Serial Number | Purchase Date | Purchase Location | Description     | First Name | Last Name | Email             | Verify Email      | Phone        | Street Address 1 | Street Address 2 | City   | Country |
   | 12345         | Sep 5, 2023   | online            | buying stroller | Magbule    | Bamja     | magbule@gmail.com | magbule@gmail.com | +35534456765 | Rruga Tirana     | Rruga Prishtina  | Tirana |Albania  |
